302e200c54b8c8d4bac16f1296a065b7da337ee4
Jonas Heschl <heschl@protonmail.com>
2024-01-24 09:45:43 +0100

    Reverse blog post order

--- src/components/blogPosts.js
+++ src/components/blogPosts.js
@@ -1,6 +1,9 @@
 import { getCollection, getEntry } from 'astro:content';
 
-export const blogPosts = await getCollection('docs', ({slug}) => {
+const blogPostsReversed = await getCollection('docs', ({slug}) => {
     const pathSections = slug.split('/');
     return pathSections[0] === 'blog' && pathSections[1] != null;
 });
+
+// We want the latest blog posts first
+export const blogPosts = blogPostsReversed.reverse();
