111207685137eee47dfe22ac1fdb6ada8a08ce81
Jonas Heschl <jonas@passgenau.digital>
2024-01-29 14:08:11 +0100

    Fix images not rendered in production build

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,15 +4,16 @@ import { docsSchema } from "@astrojs/starlight/schema";
 export const collections = {
   docs: defineCollection({
     schema: docsSchema({
-      extend: z.object({
-        image: z
-          .object({
-            src: z.string(),
-            alt: z.string(),
-          })
-          .optional(),
-        tags: z.array(z.string()).length(3).optional(),
-      }),
+      extend: ({ image }) =>
+        z.object({
+          image: z
+            .object({
+              src: image(),
+              alt: z.string(),
+            })
+            .optional(),
+          tags: z.array(z.string()).length(3).optional(),
+        }),
     }),
   }),
 };
